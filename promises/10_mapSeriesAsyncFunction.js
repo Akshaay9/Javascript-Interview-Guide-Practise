522a35978604630e77c67c1e05d377aa8c61c741
Akshaay9 <nair.akshay9898@gmail.com>
2023-01-27 11:50:59 +0530

    mapSeries Array

--- promises/10_mapSeriesAsyncFunction.js
+++ promises/10_mapSeriesAsyncFunction.js
@@ -4,65 +4,6 @@
 // rejects it if any error occurs. The inputs are run in a sequence that is
 // one after another
 
-const chopArr = (arr, num) => {
-  let result = [];
-  let tempResult = [];
-  for (let i = 0; i < arr.length; i++) {
-    const currNum = arr[i];
-    tempResult.push(currNum);
-    if ((i + 1) % num === 0 && i !== 0) {
-      result.push(tempResult);
-      tempResult = [];
-      chopCount = 1;
-    }
-  }
-  result.push(tempResult);
-  return result;
-};
-
-const mapSeries = (arr, callBackFun) => {
-  return new Promise((res, rej) => {
-    const final = arr.reduce((acc, ele) => {
-      return acc.then((accProm) => {
-        return new Promise((res, rej) => {
-          callBackFun(ele, (status, result) => {
-            if (status) {
-              res(ele);
-            } else {
-              rej(status, "error");
-            }
-          });
-        });
-      });
-    }, Promise.resolve());
-
-    final
-      .then((ele) => {
-        res(ele);
-      })
-      .catch((err) => {
-        rej(err);
-      });
-  });
-};
-
-let numPromise = mapSeries([1, 2, 3, 4, 5], function (num, callback) {
-  setTimeout(function () {
-    num = num * 2;
-    console.log(num);
-    // throw error
-    if (num === 12) {
-      callback(true);
-    } else {
-      callback(false, num);
-    }
-  }, 1000);
-});
-
-numPromise
-  .then((result) => console.log("success:" + result))
-  .catch((err) => console.log("no success", err));
-
 //  PRACTISE PROBLEM IMPORTANT
 
 const a = (cb) =>
@@ -70,7 +11,7 @@ const a = (cb) =>
     console.log("a");
     cb("a");
   }, 1000);
-  
+
 const b = (cb) =>
   setTimeout(() => {
     console.log("b");
@@ -105,8 +46,8 @@ const mapSeries1 = (arr) => {
   });
 };
 
-let numPromise1 = mapSeries([a, b, c]);
+let numPromise1 = mapSeries1([a, b, c]);
 
-numPromise
-  .then((result) => console.log("success:" + result))
+numPromise1
+  .then((result) => console.log("success:", result))
   .catch((err) => console.log("no success", err));
