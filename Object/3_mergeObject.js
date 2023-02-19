ae713b2fcfc0d7a43f3d3c11d02a465115e3ac63
Akshaay9 <nair.akshay9898@gmail.com>
2023-02-19 09:21:48 +0530

    Merge nested obj

--- Object/3_mergeObject.js
+++ Object/3_mergeObject.js
@@ -1,37 +1,34 @@
 let obj1 = {
-    name: "prashant",
-    age: 23,
-  };
-  
-  let obj2 = {
-    qualification: "BSC CS",
-    loves: "Javascript",
-  };
-  
-  const recurse = (obj, result) => {
+  name: "prashant",
+  age: 23,
+  skills: { programming: "JavaScript" },
+};
+
+let obj2 = {
+  qualification: "BSC CS",
+  loves: "Javascript",
+  skills: { sports: "swimming" },
+};
+
+const merge = (...args) => {
+  let result = {};
+
+  const recurse = (obj) => {
     for (let key in obj) {
-      if (obj.hasOwnProperty(key)) {
-        result = {
-          ...result,
-          [key]: obj[key],
-        };
+      if (typeof obj[key] === "object") {
+        result[key] = merge(result[key], obj[key]);
+      } else {
+        result[key] = obj[key];
       }
     }
     return result;
   };
-  
-  const merge = (...args) => {
-    let result = {};
-  
-    for (let i = 0; i < args.length; i++) {
-      result = {
-        ...result,
-        ...recurse(args[i], result),
-      };
-    }
-    return result;
-  };
-  
-  let merged = merge(obj1, obj2);
-  console.log(merged);
-  
+
+  for (let i = 0; i < args.length; i++) {
+    recurse(args[i]);
+  }
+  return result;
+};
+
+let merged = merge(obj1, obj2);
+console.log(merged);
