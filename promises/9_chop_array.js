0b6afcbaac3c8ea7fb4bc070e8ba2c153e8481a8
Akshay Nair <72427030+Akshaay9@users.noreply.github.com>
2023-05-23 07:20:10 +0530

    chop arr

--- promises/9_chop_array.js
+++ promises/9_chop_array.js
@@ -16,3 +16,29 @@ const chopArr = (arr, num) => {
   
 //   console.log(chopArr([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11], 3));
   
+
+// OROROROOR/ LEETCODE VERSION
+ 
+var chunk = function (arr, size) {
+  if (arr.length == 0) {
+    return [];
+  }
+  let result = [];
+  let tempResult = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (i % size === 0 && i !== 0) {
+      result.push(tempResult);
+      tempResult = [];
+      tempResult.push(arr[i]);
+    } else {
+      tempResult.push(arr[i]);
+    }
+  }
+
+  result.push(tempResult);
+
+  return result;
+};
+
+console.log(chunk([], 1));
