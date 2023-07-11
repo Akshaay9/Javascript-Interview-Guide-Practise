79a18b08458685c51dbb14c4df8d06bf186250ac
Akshay Nair <72427030+Akshaay9@users.noreply.github.com>
2023-07-11 07:04:17 +0530

    leading

---
+++ utility/1.1_Dobounce_leading.js
@@ -0,0 +1,28 @@
+//  Normal debounce method is called trailing debounce method
+
+//  Leading doubouncing
+
+const leadingDebounce = (fn, timer) => {
+    let timerid = null;
+    return function (...args) {
+      if (timerid === null) {
+        fn.call(this, ...args);
+      }
+      clearTimeout(timerid);
+      timerid = setTimeout(() => {
+        fn.call(this, ...args);
+        timerid = null;
+      }, timer);
+    };
+  };
+  
+  const sayHello = (arg) => {
+    console.log("hello", arg);
+  };
+  
+  const leadingDB = leadingDebounce(sayHello, 2000);
+  leadingDB(1);
+  leadingDB(2);
+  leadingDB(3);
+  leadingDB(4);
+  leadingDB(6);
